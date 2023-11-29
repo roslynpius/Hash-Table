b27ed417560a2e8cedb346001662b26dbd18d490
roslynpius <roslyn.pius@gmail.com>
2023-11-29 12:21:44 +0530

    Initial commit

--- src/com/bridgelabz/hashtable/Main.java
+++ src/com/bridgelabz/hashtable/Main.java
@@ -1,6 +1,8 @@
 package com.bridgelabz.hashtable;
 
+//Main class
 public class Main {
+    //main function
     public static void main(String[] args)
     {
         System.out.println("Welcome to Hash Table Implementation");
